c93de0715f71e0d72577337a4d7f900fec30927d
Myzwer <josh@onefortyfivedesign.com>
2021-02-16 15:56:15 -0500

    :hammer: Updated the header to not have any code since it isn't necessary for a 1 page website.

--- devwp/contact.php
+++ devwp/contact.php
@@ -17,10 +17,6 @@ get_header(); ?>
     <div class="full-width main-background">
         <div class = "grid-container">
             <div class="grid-x grid-padding-x padding-outer">
-                <div class="small-12 center cell">
-                    <h3>Contact</h3>
-                    <p>Look at my form, my form is amazing.</p>
-                </div>
                 <div class="small-12 medium-10 cell">
                     <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
                         the_content();
--- devwp/event.php
+++ devwp/event.php
@@ -1,1 +1,28 @@
 <?php
+/**
+ * Template Name: Event Template
+ *
+ * The Contact Page of the Yestau Theme
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Yestau
+ * @since 1.0.0
+ */
+
+get_header(); ?>
+
+    <div class="full-width main-background">
+        <div class = "grid-container padding-top">
+            <div class="grid-x grid-padding-x padding-outer">
+                <div class="small-12 medium-10 cell">
+                    <div style="width:100%; height: 500px;">
+                        <iframe src="http://lets-go.local?event=43&ticket_selector=iframe" width="100%" height="100%"></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php get_footer();

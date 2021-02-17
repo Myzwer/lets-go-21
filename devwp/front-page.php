20e6960a0820cf68e3c2820f6bb7031c2778bb87
Myzwer <josh@onefortyfivedesign.com>
2021-02-17 13:34:31 -0500

    :rocket: Finished the rough layout for the why / about section on the homepage.

--- devwp/front-page.php
+++ devwp/front-page.php
@@ -27,5 +27,37 @@ get_header(); ?>
         </div>
     </div>
 
+    <div class="grid-container full-width">
+        <div class="grid-x grid-padding-x">
+            <div class="small-12 cell">
+                <h2>What is Let's Go?</h2>
+            </div>
+
+            <div class="small-12 medium-4 cell">
+                <div class="glyphs center">
+                    <img src="http://lets-go.local/wp-content/uploads/2021/02/Buildings-model-isometric-vector-material-02-copy.png" alt="">
+                </div>
+                <h3 class = "center">Its a Movement.</h3>
+                <p>Proin maximus sem ut justo lacinia, quis posuere nisl pretium. Integer interdum vulputate pharetra. Curabitur pellentesque, neque ac dapibus lobortis, justo orci tempor leo, eu finibus leo mauris id velit.</p>
+            </div>
+
+            <div class="small-12 medium-4 cell margin-top padding-top">
+                <div class="glyphs center">
+                    <img src="http://lets-go.local/wp-content/uploads/2021/02/Buildings-model-isometric-vector-material-02-copy.png" alt="">
+                </div>
+                <h3 class = "center">Its a Movement.</h3>
+                <p>Proin maximus sem ut justo lacinia, quis posuere nisl pretium. Integer interdum vulputate pharetra. Curabitur pellentesque, neque ac dapibus lobortis, justo orci tempor leo, eu finibus leo mauris id velit.</p>
+            </div>
+
+            <div class="small-12 medium-4 cell">
+                <div class="glyphs center">
+                    <img src="http://lets-go.local/wp-content/uploads/2021/02/Buildings-model-isometric-vector-material-02-copy.png" alt="">
+                </div>
+                <h3 class = "center">Its a Movement.</h3>
+                <p>Proin maximus sem ut justo lacinia, quis posuere nisl pretium. Integer interdum vulputate pharetra. Curabitur pellentesque, neque ac dapibus lobortis, justo orci tempor leo, eu finibus leo mauris id velit.</p>
+            </div>
+        </div>
+    </div>
+
 
 <?php get_footer();

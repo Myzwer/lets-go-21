70f8fdbda310cd55a04d6ae3819f6cafbf21f8e0
Myzwer <josh@onefortyfivedesign.com>
2021-02-18 01:00:02 -0500

    :rocket: Updated the header and middle sections on the frontpage to work better across all device sizes.

--- devwp/front-page.php
+++ devwp/front-page.php
@@ -14,15 +14,22 @@
 get_header(); ?>
 
     <div class="grid-container full-width">
-        <div class="grid-x grid-padding-x full-background"
-             style="background: url(http://lets-go.local/wp-content/uploads/2021/02/Background-Cropped-min.png);  background-position: bottom left;">
-            <div class="small-12 medium-8 large-offset-1 large-6 padding-top cell">
+        <div class="grid-x grid-padding-x blue-background padding-top ">
+            <div class="small-12 medium-8 large-offset-1 large-7 cell ">
                 <h1>Let's Go</h1>
                 <h3 class = "dark-color-invert">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h3>
             </div>
 
-            <div class="small-10 small-offset-1 medium-4 medium-offset-0 header-margin">
+            <div class="small-10 small-offset-1 medium-4 medium-offset-0 large-3 padding-bottom">
                 <img src="http://lets-go.local/wp-content/uploads/2021/02/Buildings-model-isometric-vector-material-02-copy.png" alt="">
+            </div>
+        </div>
+    </div>
+
+    <div class="full-width">
+        <div class="grid-x ">
+            <div class="small-12 low-z-index cell">
+                <img class = "waves" src="http://lets-go.local/wp-content/uploads/2021/02/Header-Small.png" alt="">
             </div>
         </div>
     </div>
@@ -30,31 +37,50 @@ get_header(); ?>
     <div class="grid-container full-width">
         <div class="grid-x grid-padding-x">
             <div class="small-12 cell">
-                <h2>What is Let's Go?</h2>
+                <h2 class = "center">What is Let's Go?</h2>
             </div>
 
             <div class="small-12 medium-4 cell">
                 <div class="glyphs center">
                     <img src="http://lets-go.local/wp-content/uploads/2021/02/Buildings-model-isometric-vector-material-02-copy.png" alt="">
                 </div>
-                <h3 class = "center">Its a Movement.</h3>
-                <p>Proin maximus sem ut justo lacinia, quis posuere nisl pretium. Integer interdum vulputate pharetra. Curabitur pellentesque, neque ac dapibus lobortis, justo orci tempor leo, eu finibus leo mauris id velit.</p>
+                <h3 class = "center padding-top-small">What</h3>
+                <p class = "no-padding lr-padding margin-bottom">Let’s Go is a movement of FC people serving in our community to show love and support as we partner with organizations and groups right where we live. </p>
             </div>
 
-            <div class="small-12 medium-4 cell margin-top padding-top">
+            <div class="small-12 medium-4 cell middle-glyph-push">
                 <div class="glyphs center">
                     <img src="http://lets-go.local/wp-content/uploads/2021/02/Buildings-model-isometric-vector-material-02-copy.png" alt="">
                 </div>
-                <h3 class = "center">Its a Movement.</h3>
-                <p>Proin maximus sem ut justo lacinia, quis posuere nisl pretium. Integer interdum vulputate pharetra. Curabitur pellentesque, neque ac dapibus lobortis, justo orci tempor leo, eu finibus leo mauris id velit.</p>
+                <h3 class = "center padding-top-small">WHere</h3>
+                <p class = "no-padding lr-padding margin-bottom">We will serve throughout Blount and Knox County at area organizations and also at locations you select through our Choose Your Own Adventure option.</p>
             </div>
 
             <div class="small-12 medium-4 cell">
                 <div class="glyphs center">
                     <img src="http://lets-go.local/wp-content/uploads/2021/02/Buildings-model-isometric-vector-material-02-copy.png" alt="">
                 </div>
-                <h3 class = "center">Its a Movement.</h3>
-                <p>Proin maximus sem ut justo lacinia, quis posuere nisl pretium. Integer interdum vulputate pharetra. Curabitur pellentesque, neque ac dapibus lobortis, justo orci tempor leo, eu finibus leo mauris id velit.</p>
+                <h3 class = "center padding-top-small">How</h3>
+                <p class = "no-padding lr-padding margin-bottom">You can be part of Let’s Go by choosing an area to serve as an individual, family, or small group! Just sign up, and then show up at the location that you registered for on the day/time you selected-ready to help, smile, encourage, and make a difference.</p>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="full-width">
+        <div class="grid-x ">
+            <div class="small-12 cell">
+                <img class = "waves" src="http://lets-go.local/wp-content/uploads/2021/02/Vector.png" alt="">
+            </div>
+        </div>
+    </div>
+
+    <div class="blue-background tiny-margin-pull full-width">
+        <div class="grid-x grid-container">
+            <div class="small-12 cell">
+                <h2 class = "center dark-color-invert">Serving OPS</h2>
+                <h3 class = "center dark-color-invert">Notice</h3>
+                <p class = "center dark-color-invert">Proin maximus sem ut justo lacinia, quis posuere nisl pretium. Integer interdum vulputate pharetra. Curabitur pellentesque, neque ac dapibus.</p>
             </div>
         </div>
     </div>

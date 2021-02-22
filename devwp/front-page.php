6c98fa588a73fcca757990b28ad5581a65159acd
Myzwer <josh@onefortyfivedesign.com>
2021-02-22 01:21:01 -0500

    :construction: Added ACF to the whole page except for the lower cards, though I did verify they would work for the first one. Aside from adding those ACF cards (and any tweaks the come with that), this page is finished.

--- devwp/front-page.php
+++ devwp/front-page.php
@@ -16,14 +16,14 @@ get_header(); ?>
     <div class="grid-container full-width">
         <div class="grid-x grid-padding-x blue-background padding-top ">
             <div class="small-12 medium-8 large-offset-1 large-7 cell ">
-                <img class="padding-bottom" src="http://lets-go.local/wp-content/uploads/2021/02/LETS_GO_LOGO.png"
-                     alt="">
-                <h3 class="dark-color-invert">Making It Count In Our Community. </h3>
+                <img class="padding-bottom" src="<?php the_field('logo_image'); ?>"
+                     alt="Let's Go Logo">
+                <h3 class="dark-color-invert"><?php the_field('subtext'); ?></h3>
             </div>
 
             <div class="small-10 small-offset-1 medium-4 medium-offset-0 large-3 padding-bottom">
-                <img src="http://lets-go.local/wp-content/uploads/2021/02/Buildings-model-isometric-vector-material-02-copy.png"
-                     alt="">
+                <img src="<?php the_field('flavor_image'); ?>"
+                     alt="Car driving through isometric city">
             </div>
         </div>
     </div>
@@ -31,7 +31,7 @@ get_header(); ?>
     <div class="full-width">
         <div class="grid-x ">
             <div class="small-12 low-z-index cell">
-                <img class="waves" src="http://lets-go.local/wp-content/uploads/2021/02/Header-Small.png" alt="">
+                <img class="waves" src="<?php the_field('section_background_image'); ?>" alt="">
             </div>
         </div>
     </div>
@@ -39,40 +39,34 @@ get_header(); ?>
     <div class="grid-container full-width">
         <div class="grid-x grid-padding-x">
             <div class="small-12 cell">
-                <h2 class="center">What is Let's Go?</h2>
+                <h2 class="center"><?php the_field('about_title'); ?></h2>
             </div>
 
             <div class="small-12 medium-4 cell">
                 <div class="glyphs center">
-                    <img src="http://lets-go.local/wp-content/uploads/2021/02/Homepage-What.png"
-                         alt="">
+                    <img src="<?php the_field('section_1_image'); ?>"
+                         alt="Isometric People Serving">
                 </div>
-                <h3 class="center padding-top-small">What</h3>
-                <p class="no-padding lr-padding margin-bottom">Let’s Go is a movement of FC people serving in our
-                    community to show love and support as we partner with organizations and groups right where we
-                    live. </p>
+                <h3 class="center padding-top-small"><?php the_field('section_1_title'); ?></h3>
+                <p class="no-padding lr-padding margin-bottom"><?php the_field('section_1_body'); ?> </p>
             </div>
 
             <div class="small-12 medium-4 cell middle-glyph-push">
                 <div class="glyphs center">
-                    <img src="http://lets-go.local/wp-content/uploads/2021/02/Homepage-Where.png"
-                         alt="">
+                    <img src="<?php the_field('section_2_image'); ?>"
+                         alt="Isometric Cityscape">
                 </div>
-                <h3 class="center padding-top-small">WHere</h3>
-                <p class="no-padding lr-padding margin-bottom">We will serve throughout Blount and Knox County at area
-                    organizations and also at locations you select through our Choose Your Own Adventure option.</p>
+                <h3 class="center padding-top-small"><?php the_field('section_2_title'); ?></h3>
+                <p class="no-padding lr-padding margin-bottom"><?php the_field('section_2_body'); ?> </p>
             </div>
 
             <div class="small-12 medium-4 cell">
                 <div class="glyphs center">
-                    <img src="http://lets-go.local/wp-content/uploads/2021/02/Homepage-How.png"
-                         alt="">
+                    <img src="<?php the_field('section_3_image'); ?>"
+                         alt="Isometric People moving boxes">
                 </div>
-                <h3 class="center padding-top-small">How</h3>
-                <p class="no-padding lr-padding margin-bottom">You can be part of Let’s Go by choosing an area to serve
-                    as an individual, family, or small group! Just sign up, and then show up at the location that you
-                    registered for on the day/time you selected-ready to help, smile, encourage, and make a
-                    difference.</p>
+                <h3 class="center padding-top-small"><?php the_field('section_3_title'); ?></h3>
+                <p class="no-padding lr-padding margin-bottom"><?php the_field('section_3_body'); ?></p>
             </div>
         </div>
     </div>
@@ -81,7 +75,7 @@ get_header(); ?>
     <div class="full-width">
         <div class="grid-x ">
             <div class="small-12 cell">
-                <img class="waves" src="http://lets-go.local/wp-content/uploads/2021/02/Vector.png" alt="">
+                <img class="waves" src="<?php the_field('footer_image'); ?>" alt="">
             </div>
         </div>
     </div>
@@ -89,12 +83,10 @@ get_header(); ?>
     <div class="blue-background serve-margin-pull full-width">
         <div class="grid-x grid-container">
             <div class="small-12 cell">
-                <h2 class="center dark-color-invert padding-bottom small-font-mobile">Serving Opportunties</h2>
+                <h2 class="center dark-color-invert padding-bottom small-font-mobile"><?php the_field('serving_ops_title'); ?></h2>
                 <div class="orange-background rounded-corners margin-bottom">
-                    <h3 class="center dark-color-invert padding-top">Notice</h3>
-                    <p class="dark-color-invert no-spacing lr-padding padding-bottom">Please do not bring any extra
-                        people to serve who have not registered for the event. Although it sounds helpful, our serve
-                        organizations have specified the number of people allowed and we want to honor this.</p>
+                    <h3 class="center dark-color-invert padding-top"><?php the_field('alert_box_header'); ?></h3>
+                    <p class="dark-color-invert no-spacing lr-padding padding-bottom"><?php the_field('alert_box_copy'); ?></p>
                 </div>
             </div>
         </div>
@@ -107,64 +99,58 @@ get_header(); ?>
 
 
                     <!-- ************ ROW 1 ************* -->
-                    <div class="small-12 cell card-margin-bottom "><!-- FEATURED Outer -->
-                        <div class="grid-x lock-tall"><!-- Card 1 Inner / Height Locks -->
+                    <?php if (have_rows('event_1')): ?>
+                        <?php while (have_rows('event_1')): the_row(); ?>
+                            <div class="small-12 cell card-margin-bottom "><!-- FEATURED Outer -->
+                                <div class="grid-x lock-tall"><!-- Card 1 Inner / Height Locks -->
 
-                            <!-- Left / Top -->
-                            <div class="small-12 medium-4 cell gradient-1 gradient-corners relative">
-                                <div class="card-center">
-                                    <img src="http://lets-go.local/wp-content/uploads/2021/02/KARM.png" alt="">
-                                </div>
-                            </div>
-
-                            <!-- Right / Bottom -->
-                            <div class="small-12 medium-8 cell card-contents content-corners relative">
-                                <div class="card-content-center">
-                                    <h2 class = "no-spacing lr-padding">Karm Stores</h2>
-                                    <p class = "no-spacing lr-padding margin-bottom">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias asperiores
-                                        aspernatur beatae ducimus eaque eius est et ex facere fugit labore, natus
-                                        obcaecati, quis quisquam recusandae reprehenderit repudiandae sit, tempore.</p>
-                                    <div class="grid-x icon-box">
-                                        <div class="small-4 cell off">
-                                            <div class="center">
-                                                <img class="center"
-                                                     src="https://img.icons8.com/pastel-glyph/40/000000/check-file--v3.png"/>
-                                            </div>
-                                            <p class="center no-spacing">Background Check</p>
-                                        </div>
-                                        <div class="small-4 cell">
-                                            <div class="center">
-                                                <img class="center"
-                                                     src="https://img.icons8.com/ios/40/000000/children--v1.png"/>
-                                            </div>
-                                            <p class="center no-spacing">Family Friendly</p>
-                                        </div>
-                                        <div class="small-4 cell off">
-                                            <div class="center">
-                                                <img class="center"
-                                                     src="https://img.icons8.com/ios/40/000000/protection-mask.png"/>
-                                            </div>
-                                            <p class="center no-spacing">Mask Required</p>
+                                    <!-- Left / Top -->
+                                    <div class="small-12 medium-4 cell gradient-corners relative"
+                                         style="background: <?php the_sub_field('event_gradient'); ?>;">
+                                        <div class="card-center">
+                                            <img src="<?php the_sub_field('event_image'); ?>" alt="Event Graphic">
                                         </div>
                                     </div>
 
-                                    <div class="center margin-bottom">
-                                        <a href="#"></a>
-                                        <button class="btn btn-v1 register-button">Register</button>
+                                    <!-- Right / Bottom -->
+                                    <div class="small-12 medium-8 cell card-contents content-corners relative">
+                                        <div class="card-content-center">
+                                            <h2 class="no-spacing lr-padding"><?php the_sub_field('event_title'); ?></h2>
+                                            <p class="no-spacing lr-padding margin-bottom"><?php the_sub_field('event_text'); ?></p>
+                                            <div class="grid-x icon-box">
+                                                <div class="small-4 cell <?php the_sub_field('background_check'); ?>">
+                                                    <div class="center">
+                                                        <img class="center"
+                                                             src="https://img.icons8.com/pastel-glyph/40/000000/check-file--v3.png"/>
+                                                    </div>
+                                                    <p class="center no-spacing">Background Check</p>
+                                                </div>
+                                                <div class="small-4 cell <?php the_sub_field('family_friendly'); ?>">
+                                                    <div class="center">
+                                                        <img class="center"
+                                                             src="https://img.icons8.com/ios/40/000000/children--v1.png"/>
+                                                    </div>
+                                                    <p class="center no-spacing">Family Friendly</p>
+                                                </div>
+                                                <div class="small-4 cell <?php the_sub_field('mask_required'); ?>">
+                                                    <div class="center">
+                                                        <img class="center"
+                                                             src="https://img.icons8.com/ios/40/000000/protection-mask.png"/>
+                                                    </div>
+                                                    <p class="center no-spacing">Mask Required</p>
+                                                </div>
+                                            </div>
+
+                                            <div class="center margin-bottom">
+                                                <a href="#"></a>
+                                                <button class="btn btn-v1 register-button">Register</button>
+                                            </div>
+                                        </div>
                                     </div>
-
-
-                                </div>
-                            </div>
-                        </div><!-- Card 3 Inner End -->
-                    </div><!-- Card 3 Outer End -->
-
-
-
-
-
-
-
+                                </div><!-- Card 3 Inner End -->
+                            </div><!-- Card 3 Outer End -->
+                        <?php endwhile; ?>
+                    <?php endif; ?>
 
 
                     <!-- ************ ROW 2 ************* -->
@@ -223,14 +209,6 @@ get_header(); ?>
                     </div><!-- Card 3 Outer End -->
 
 
-
-
-
-
-
-
-
-
                     <!-- ************ ROW 3 ************* -->
                     <div class="small-12 medium-6 cell card-margin-bottom"><!-- Card 3 TALL Outer -->
                         <div class="grid-x lock-tall-half"><!-- Card 2 Inner / Height Locks -->
@@ -261,14 +239,6 @@ get_header(); ?>
                             </div>
                         </div><!-- Card 3 Inner End -->
                     </div><!-- Card 3 Outer End -->
-
-
-
-
-
-
-
-
 
 
                     <!-- ************ ROW 4 ************* -->
@@ -316,15 +286,6 @@ get_header(); ?>
                             </div>
                         </div><!-- Card 3 Inner End -->
                     </div><!-- Card 3 Outer End -->
-
-
-
-
-
-
-
-
-
 
 
                     <!-- ************ ROW 5 ************* -->

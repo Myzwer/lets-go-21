989e9eee5724035c24318b0cd9702dfbe3daef6b
Myzwer <josh@onefortyfivedesign.com>
2021-02-23 01:14:58 -0500

    :rocket: Completed the design of the events page, will need to convert it to ACF.

--- devwp/event.php
+++ devwp/event.php
@@ -37,7 +37,7 @@ get_header(); ?>
     <div class="full-width">
         <div class="grid-x ">
             <div class="small-12 cell">
-                <img class="waves" src="http://lets-go.local/wp-content/uploads/2021/02/Vector.png" alt="">
+                <img class="waves" src="http://lets-go.local/wp-content/uploads/2021/02/Vector-Top-Small.png" alt="">
             </div>
         </div>
     </div>
@@ -113,7 +113,7 @@ get_header(); ?>
     <div class="full-width main-background">
         <div class="grid-container">
             <div class="grid-x grid-padding-x padding-outer">
-                <div class="small-12 medium-10 cell">
+                <div class="small-12 cell">
                     <?php if (have_posts()) : while (have_posts()) : the_post();
                         the_content();
                     endwhile;

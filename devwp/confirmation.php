932932f77a84b0fe89107e0db7ce186fbe5cd003
Myzwer <josh@onefortyfivedesign.com>
2021-02-23 11:59:14 -0500

    :heavy-plus-sign: Added confirmation page that users will hit after they complete their registration.

---
+++ devwp/confirmation.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Template Name: Event Template
+ *
+ * The Contact Page of the Let's Go Theme
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Let's Go
+ * @since 1.0.0
+ */
+
+get_header(); ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php get_footer();

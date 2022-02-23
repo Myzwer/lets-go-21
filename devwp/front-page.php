bd1f9b998a53e1b6134ddd1a012611ef1afcc015
Myzwer <josh@onefortyfivedesign.com>
2022-02-23 16:04:00 -0500

    :construction: Got a card working in the lower section enough that its worth a commit, but it's far from done.

--- devwp/front-page.php
+++ devwp/front-page.php
@@ -21,11 +21,12 @@ get_header(); ?>
     <div class="full-width main-background">
         <div class="grid-x mobile-homepage-header">
             <div class="small-12 medium-offset-1 medium-4 cell z-3 relative homepage-header">
-                <img class = "mobile-padding" src="https://lets-go.local/wp-content/uploads/2022/02/Asset-5.png" alt="">
+                <img class="mobile-padding" src="https://lets-go.local/wp-content/uploads/2022/02/Asset-5.png" alt="">
             </div>
             <div class="small-12 medium-offset-1 medium-5 z-3 relative">
                 <div class="content-middle w-full">
-                    <h1 class = "lr-padding">Some text and stuff about a brandscript of lets go asking you to do things.</h1>
+                    <h1 class="lr-padding">Some text and stuff about a brandscript of lets go asking you to do
+                        things.</h1>
                     <div class="lr-padding">
                         <button class="btn btn-v1">Register Now</button>
                     </div>
@@ -41,7 +42,7 @@ get_header(); ?>
         <div class="grid-x margin-bottom">
             <div class="small-12 center cell">
                 <div class="drop-effect">
-                <h2>What is Let's Go?</h2>
+                    <h2>What is Let's Go?</h2>
                 </div>
             </div>
         </div>
@@ -49,9 +50,10 @@ get_header(); ?>
             <div class="grid-x grid-margin-x">
                 <div class="small-12 medium-4 cell padding-bottom">
                     <div class="center">
-                        <img class = "w-half padding-bottom" src="https://lets-go.local/wp-content/uploads/2022/02/Icon-1.png" alt="">
+                        <img class="w-half padding-bottom"
+                             src="https://lets-go.local/wp-content/uploads/2022/02/Icon-1.png" alt="">
                     </div>
-                        <h3 class="dark-color-invert">Look Up</h3>
+                    <h3 class="dark-color-invert">Look Up</h3>
                     <p class="no-spacing dark-color-invert">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                         Culpa ex illum ipsam ipsum labore, nihil nisi numquam officiis quaerat, rerum sequi soluta sunt?
                         Aliquam at dolore harum ut, veniam vitae.</p>
@@ -59,7 +61,8 @@ get_header(); ?>
 
                 <div class="small-12 medium-4 cell middle-icon-push padding-bottom">
                     <div class="center">
-                        <img class = "w-half padding-bottom" src="https://lets-go.local/wp-content/uploads/2022/02/Icon-1.png" alt="">
+                        <img class="w-half padding-bottom"
+                             src="https://lets-go.local/wp-content/uploads/2022/02/Icon-1.png" alt="">
                     </div>
                     <h3 class="dark-color-invert">Look Up</h3>
                     <p class="no-spacing dark-color-invert">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -69,7 +72,8 @@ get_header(); ?>
 
                 <div class="small-12 medium-4 cell padding-bottom">
                     <div class="center">
-                        <img class = "w-half padding-bottom" src="https://lets-go.local/wp-content/uploads/2022/02/Icon-1.png" alt="">
+                        <img class="w-half padding-bottom"
+                             src="https://lets-go.local/wp-content/uploads/2022/02/Icon-1.png" alt="">
                     </div>
                     <h3 class="dark-color-invert">Look Up</h3>
                     <p class="no-spacing dark-color-invert">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -80,18 +84,75 @@ get_header(); ?>
         </div>
     </div>
 
-    <div class="full-width orange-background">
+    <div class="full-width orange-background dark-color-invert">
         <img src="https://lets-go.local/wp-content/uploads/2022/02/About-Bottom-Wave.png" alt="">
     </div>
     <div class="full-width blue-background">
-        <div class="grid-x">
+        <div class="grid-x grid-container">
             <div class="small-12 center cell">
-                <h3>Serving Opportunities</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus eaque facilis recusandae. At
-                    eligendi et fugit vero. Beatae illum minus obcaecati, possimus reprehenderit rerum similique veniam.
-                    Aliquid esse porro possimus.</p>
+                <div class="drop-effect add-padding">
+                    <h2>Serving Opportunities</h2>
+                </div>
+                <div class="orange-background add-padding rounded-corners box-shadow">
+                    <h3 class="dark-color-invert center uppercase">Please Note</h3>
+                    <p class="dark-color-invert">Please do not bring any extra people to serve who have not registered
+                        for the event. Although it sounds helpful, our serve organizations have specified the number of
+                        people allowed and we want to honor this.</p>
+                </div>
             </div>
         </div>
-    </div>
+
+        <!-- START CARDS -->
+
+
+        <div class="grid-x card-outer center add-padding"><!-- Outer -->
+            <div class="small-12 center cell"><!-- Outer Cell -->
+
+
+                <div class="grid-x margin-top"><!-- Card 1 Outer-->
+                    <div class="small-12 cell"><!-- Card 1 Outer - Sets outer width of card, how wide it is on site.-->
+
+                        <div class="grid-x margin-top box-shadow"><!-- Card 1 Inner -->
+                            <div class="small-12 medium-4 cell">
+                                <div class="orange-background gradient-corners">
+                                    <img class = "add-padding" src="https://lets-go.local/wp-content/uploads/2022/02/Asset-6.png" alt="">
+                                </div>
+                            </div>
+
+                            <div class="small-12 medium-8 cell main-background content-corners ">
+                                <div class=" add-padding left">
+                                    <h3>Second Harvest Food Bank</h3>
+                                    <p class="no-spacing margin-bottom">Operates a food warehouse headquarters that functions to lead
+                                        our community in the fight to end hunger. Their goal is to provide adequate
+                                        nutrition for a healthy life for adults, seniors, and children in 18 East
+                                        Tennessee counties.</p>
+                                    <div class="grid-x"><!-- Card 1 Icons -->
+                                        <div class="small-6 cell center">
+                                            <img src="https://img.icons8.com/ios/40/000000/children--v1.png" alt="">
+                                            <p> Family <br /> Friendly</p>
+                                        </div>
+                                        <div class="small-6 cell center">
+                                            <img src="https://img.icons8.com/ios/40/000000/children--v1.png" alt="">
+                                            <p> Family <br /> Friendly</p>
+                                        </div>
+                                        <div class="small-12 cell ">
+                                            <div class="lr-padding ">
+                                                <button class="btn btn-v1 center">Register Now</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div> <!-- Card Inner End-->
+                        </div><!-- Card Inner End -->
+                    </div> <!-- Card 1 End -->
+                </div><!-- Card 1 End -->
+
+
+            </div><!-- Outer Cell End -->
+        </div><!-- Outer End -->
+
+        <!-- START CARDS -->
+
 
 <?php get_footer();
